f651580e7610be4e05154b30e909d69c6e9fa634
Mxnchriis <manchrisagot@gmail.com>
2025-08-06 12:12:59 -0400

    Correctifs et ajout de défilement de texte

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -23,11 +23,23 @@ function Accordion({ title, children }) {
   );
 }
 
+function Placeholder() {
+  return (
+    <>
+      <div className="defilement-lisse">
+        <div className="contenu">
+          En cours...
+        </div>
+      </div>
+    </>
+  );
+}
+
 const FigmaPrototype = () => {
   const [loaded, setLoaded] = useState(false);
 
   return (
-    <div style={{ position: 'relative', paddingTop: '100%' }}>
+    <div style={{ position: 'relative', paddingTop: '60%' }}>
       {!loaded && <div className="placeholder">Chargement du prototype...</div>}
       <iframe
         title="Prototype Figma"
@@ -54,29 +66,30 @@ function Projets() {
       <p>Quelques-uns de mes projets récents :</p>
       <ul>
         <Accordion title="Projet Angular">
-        <li>En cours...</li>
+          {/* <li>En cours...</li> */}
+          <Placeholder />
         </Accordion>
 
         <Accordion title="Projet UI/UX">
-        <h2>Gaspiypa</h2>
-        <FigmaPrototype />
-        <br />
-        <p>Gaspiypa est une application de gestion alimentaires. Elle permet aux utilisateurs de suivre et de réduire leur gaspillage alimentaire en fournissant des conseils personnalisés, des recettes et des statistiques sur leurs habitudes de consommation.</p>
-        <p>Le prototype interactif complet est disponible sur Figma. Vous pouvez l'explorer en cliquant sur le lien ci-dessous :</p>
-        <a href="https://www.figma.com/design/LoHy5usBRI598X3Cg9i2JE/UI-UX-Gaspiypa?node-id=2-89&t=Zu0LTVRSElo3BKhA-1" target="_blank" rel="noopener noreferrer">UI/UX Gaspiypa</a>
+          <h2>Gaspiypa</h2>
+          <FigmaPrototype />
+          <br />
+          <p>Gaspiypa est une application de gestion alimentaires. Elle permet aux utilisateurs de suivre et de réduire leur gaspillage alimentaire en fournissant des conseils personnalisés, des recettes et des statistiques sur leurs habitudes de consommation.</p>
+          <p>Le prototype interactif complet est disponible sur Figma. Vous pouvez l'explorer en cliquant sur le lien ci-dessous :</p>
+          <a href="https://www.figma.com/design/LoHy5usBRI598X3Cg9i2JE/UI-UX-Gaspiypa?node-id=2-89&t=Zu0LTVRSElo3BKhA-1" target="_blank" rel="noopener noreferrer">UI/UX Gaspiypa</a>
         </Accordion>
 
         <Accordion title="Projet Gaming">
-        <h2>Cat'N Dog</h2>
-        <div className="boximg">
-          <img src= {screengame2} alt="Cat'N Dog screen1" className='screengame'/>
-          <img src={screengame3} alt="Cat'N Dog screen2" className='screengame'/>
-        </div>
-        <p>Cat'N Dog est un jeu de fight-platformer où les joueurs incarnent un chat et un chien dans un univers coloré et dynamique.
-          Le but du jeu est de voler la croquette adverse, pour cela vous devez éliminer votre adversaire pour débloquer l'accès au différente zone donnant accès à la croquette.
-          Le jeu propose des combats en arène avec des mécaniques de plateforme, permettant aux joueurs de sauter, courir et combattre dans des environnements variés.</p>
-        <br />
-        <p>Télécharger la démo : (Pas encore disponible)</p>
+          <h2>Cat'N Dog</h2>
+          <div className="boximg">
+            <img src= {screengame2} alt="Cat'N Dog screen1" className='screengame'/>
+            <img src={screengame3} alt="Cat'N Dog screen2" className='screengame'/>
+          </div>
+          <p>Cat'N Dog est un jeu de fight-platformer où les joueurs incarnent un chat et un chien dans un univers coloré et dynamique.
+            Le but du jeu est de voler la croquette adverse, pour cela vous devez éliminer votre adversaire pour débloquer l'accès au différente zone donnant accès à la croquette.
+            Le jeu propose des combats en arène avec des mécaniques de plateforme, permettant aux joueurs de sauter, courir et combattre dans des environnements variés.</p>
+          <br />
+          <p>Télécharger la démo : (Pas encore disponible)</p>
         </Accordion>
 
         <p>Voici également quelques sites web sur lequel j'ai pu travaillé :</p>
