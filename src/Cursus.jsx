767c1055ef66362f31c3a87025d1f66d861bbd98
Mxnchriis <manchrisagot@gmail.com>
2025-10-03 16:53:03 +0200

    Début de mise à jour de la section "Parcours"

--- src/Cursus.jsx
+++ src/Cursus.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './Cursus.css'
 import githubLogo from './assets/contact/github.png'
+import etudeLogo from './assets/img/etudeW.png'
+import devLogo from './assets/img/terminalW.png'
+import proLogo from './assets/img/pro.png'
 
 
 function Cursus(){
@@ -8,37 +11,150 @@ function Cursus(){
         <section id="parcours">
             <div className="Bloc-content">
                 <h1>Mon <span id='Colored'>parcours</span></h1>
-                <p>J'ai suivi un parcours académique atypique en choisissant un baccalauréat général avec les spécialités NSI, LLCER Anglais et SES.
-                    L'informatique et l'anglais ont toujours occupé une place importante dans mon quotidien, que ce soit à travers les jeux vidéo ou l'univers musical anglophone. Ce qui me passionne avant tout, c'est la compréhension et la logique derrière les choses. Ces disciplines se sont donc imposées naturellement comme mes matières de prédilection. Le choix de la SES, quant à lui, relevait davantage d'une stratégie afin d'acquérir une vision plus large du monde professionnel.              J'ai ensuite poursuivi mes études avec une Licence Sciences du Numérique, formation pluridisciplinaire qui m'a permis d'explorer différents domaines et langages de programmation. Ce choix s'est révélé pertinent, car il m'a offert une vue d'ensemble sur les divers métiers du numérique et leurs réalités professionnelles.
-                    Aujourd'hui, je poursuis mon parcours en MBA Développeur Full Stack. Au fil de ma licence, j'ai découvert un véritable intérêt pour la création numérique (programmation, développement web, UI/UX). Ce qui me motive particulièrement, c'est la satisfaction de partir d'une idée, de la concrétiser en un programme, un site ou même un jeu, et d'obtenir un résultat fonctionnel et abouti.
-                    À terme, j'ambitionne de m'orienter vers un métier dans la création numérique, voire d'évoluer vers un projet entrepreneurial autour d'une application à forte valeur et popularité.</p>
+                <p>J'ai suivi un parcours académique atypique en choisissant un baccalauréat général avec les spécialités NSI, LLCER Anglais et SES.</p>
                 
-                {/* <div className="parcour-history">
+                <div className="parcour-history">
                     <div className="gradiation-bar"></div>
                     <div className="box-content-column" id="history-container">
         
                         <div className="box-content-row" id='period-box'>
-                            <img src={githubLogo} alt="Test" />
+                            <div className="period-circle">
+                                <img src={etudeLogo} alt="Test" />
+                            </div>
                             <div className="period-content">
-                                <h2>Le nom du diplôme</h2>
-                                <h3>Le nom de l'école</h3>
-                                <h4>La date</h4>
-                                <h4>Le lieu</h4>
+                                <h2>MBA Développeur Full-Stack</h2>
+                                <h3>MyDigitalSchool</h3>
+                                <h4>Sept 2025 - Actuellement</h4>
+                                <h4>Montpellier, 34080</h4>
+                                <p>Formation niveau 7 (BAC+5) en développement web full-stack avancé.</p>
+                                <div className="box-content-row" id="skills-list">
+                                    <li>React</li>
+                                    <li>Angular</li>
+                                    <li>Node.js</li>
+                                    <li>Docker</li>
+                                    <li>Gestion de Projet</li>
+                                    <li>Python</li>
+                                    <li>Java</li>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="box-content-row" id='period-box'>
+                            <div className="period-circle">
+                                <img src={devLogo} alt="Test" />
+                            </div>
+                            <div className="period-content">
+                                <h2>Stage de développement Web</h2>
+                                <h3>NASDY</h3>
+                                <h4>Juin 2024 - Juillet 2024</h4>
+                                <h4>FDF - Martinique</h4>
                                 <p>Description de la période</p>
                                 <div className="box-content-row" id="skills-list">
-                                    <li>VirtualBox</li>
-                                    <li>Linux (Ubuntu)</li>
-                                    <li>Windows</li>
-                                    <li>Cmd</li>
-                                    <li>PowerShell</li>
-                                    <li>SSH</li>
-                                    <li>PuTTY</li>
+                                    <li>Odoo</li>
+                                    <li>Odoo.sh</li>
+                                    <li>Création de Site</li>
+                                    <li>Base de Données</li>
+                                    <li>Migration de Site</li>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="box-content-row" id='period-box'>
+                            <div className="period-circle">
+                                <img src={proLogo} alt="Test" />
+                            </div>
+                            <div className="period-content">
+                                <h2>Promotion au statut de Directeur</h2>
+                                <h3>ISP (ICEA)</h3>
+                                <h4>Février 2024 - Octobre 2024</h4>
+                                <h4>FDF - Martinique</h4>
+                                <p>Description de la période</p>
+                                <div className="box-content-row" id="skills-list">
+                                    <li>Management des équipes</li>
+                                    <li>Responsabilité des missions</li>
+                                    <li>Réunion</li>
+                                    <li>Gestion des relation Clients</li>
+                                    <li>Rapport de missions</li>
+                                    <li>Rédaction de mails</li>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="box-content-row" id='period-box'>
+                            <div className="period-circle">
+                                <img src={proLogo} alt="Test" />
+                            </div>
+                            <div className="period-content">
+                                <h2>Création d'une junior entreprise (ISP)</h2>
+                                <h3>ISP (ICEA)</h3>
+                                <h4>Sept 2024 - Février 2024</h4>
+                                <h4>FDF - Martinique</h4>
+                                <p>Description de la période</p>
+                                <div className="box-content-row" id="skills-list">
+                                    <li>Création de documents administratifs</li>
+                                    <li>Réunion de votes</li>
+                                    <li>Crétion de logos</li>
+                                    <li>Création de Flyers</li>
+                                    <li>Gestion des réseaux sociaux</li>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="box-content-row" id='period-box'>
+                            <div className="period-circle">
+                                <img src={proLogo} alt="Test" />
+                            </div>
+                            <div className="period-content">
+                                <h2>Licence Sciences du Numérique</h2>
+                                <h3>ICEA</h3>
+                                <h4>Sept 2022 - Juin 2025</h4>
+                                <h4>FDF - Martinique</h4>
+                                <p>Formation niveau 6 (BAC+3) dans le Numérique</p>
+                                <div className="box-content-row" id="skills-list">
+                                    <li>Python</li>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>JavaScript</li>
+                                    <li>SQL</li>
+                                    <li>PHP</li>
+                                    <li>Java</li>
+                                    <li>C</li>
+                                    <li>C++</li>
+                                    <li>C#</li>
+                                    <li>Bash</li>
+                                    <li>Réseaux et sécurité</li>
+                                    <li>Système d'exploitation</li>
+                                    <li>Gestion de Projets</li>
+                                    <li>Anglais</li>
+                                    <li>UE5</li>
+                                    <li>Unity</li>
+                                    <li>VR</li>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="box-content-row" id='period-box'>
+                            <div className="period-circle">
+                                <img src={etudeLogo} alt="Test" />
+                            </div>
+                            <div className="period-content">
+                                <h2>Bac Général Spé NSI & LLCER Anglais</h2>
+                                <h3>LPO La Jetée</h3>
+                                <h4>Sept 2019 - Juin 2022</h4>
+                                <h4>Le François - Martinique</h4>
+                                <p>Baccalauréat Général</p>
+                                <div className="box-content-row" id="skills-list">
+                                    <li>Python</li>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>JavaScript</li>
+                                    <li>Anglais</li>
                                 </div>
                             </div>
                         </div>
 
                     </div>
-                </div> */}
+                </div>
 
             </div>
         </section>
